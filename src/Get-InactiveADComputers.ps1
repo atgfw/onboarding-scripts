8506015fb93949b5ed57b0c8131a6d4cf57a4171
noahe <noahe@atgfw.com>
2025-07-17 14:51:37 -0400

    Get-InactiveADComputers

---
+++ src/Get-InactiveADComputers.ps1
@@ -0,0 +1,52 @@
+function Get-InactiveADComputers() {
+    <#
+        .SYNOPSIS
+        Gets all users who haven't logged in recently.
+
+        .DESCRIPTION
+        Gets all AD users who either has a LastLogon value in the past $DaysInactive or who has LastLogonTimestamp
+        value in the past ($DaysInactive + 14 days).
+
+        Filters out names that are -like any string in $IgnoreList
+
+        .OUTPUTS
+        Microsoft.ActiveDirectory.Management.ADUser. Returns one or more user objects.
+
+    #>
+    param (
+    <#
+        .PARAMETER DaysInactive
+        Number of days of inactivity required for a user to be declared Inactive
+    #>
+        [int]$DaysInactive = 60,
+    <# 
+        .PARAMETER IgnoreList
+        List of strings to be compared against the Name of each computer with -like and filtered out
+    #>
+        [string[]]$IgnoreList = @(),
+        [string]$SearchBase = $null
+    )
+
+    $localThreshold = (Get-Date).AddDays(-$DaysInactive)
+    $replicatedThreshold = (Get-Date).AddDays(-($DaysInactive+14)) # LastLogonTimestamp synchronization delay
+
+    $filter = {
+        [DateTime]::FromFileTime($_.LastLogon) -lt $localThreshold -and
+        [DateTime]::FromFileTime($_.LastLogonTimeStamp) -lt $replicatedThreshold -and
+        $_.enabled -eq $true -and
+        ($IgnoreList -notcontains $null -and
+            ($IgnoreList | ForEach-Object { $_pattern = $_; $_.userprincipalname -notlike $_pattern }) -notcontains $false)
+    }
+
+    $params = @{
+        Filter = '*'
+        Properties = 'LastLogon', 'LastLogonTimeStamp'
+    }
+
+    if ($SearchBase) {
+        $params['SearchBase'] = $SearchBase
+    }
+
+    return Get-Computer @params |
+        Where-Object $filter
+}
